cdb5b86bf40eabc10d9a594191093f75e108d285
荒野無燈 <ttys3@outlook.com>
2020-12-05 04:05:09 +0800

    feat: support m3u8 master list

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
+build_linux:
+	env GOOS=linux GOARCH=amd64 go build -o ./bin/hlsdl ./cmd/hlsdl
+	@md5sum ./bin/hlsdl
+
 build_osx:
 	env GOOS=darwin GOARCH=amd64 go build -o ./bin/hlsdl_osx ./cmd/hlsdl
 	@md5sum ./bin/hlsdl_osx
-
-build_linux:
-	env GOOS=linux GOARCH=amd64 go build -o ./bin/hlsdl_linux ./cmd/hlsdl
-	@md5sum ./bin/hlsdl_linux
 
 build_windows:
 	env GOOS=windows GOARCH=amd64 go build -o ./bin/hlsdl_windows.exe ./cmd/hlsdl
 	@md5sum ./bin/hlsdl_windows.exe
 
 build: build_osx build_linux build_windows
+
+clean:
+	rm -f ./bin/hlsdl*
--- parser.go
+++ parser.go
@@ -2,8 +2,11 @@ package hlsdl
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"net/http"
 	"net/url"
+	"sort"
 	"strings"
 
 	"github.com/grafov/m3u8"
@@ -19,6 +22,45 @@ func parseHlsSegments(hlsURL string) ([]*Segment, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	if t == m3u8.MASTER {
+		if masterpl, ok := p.(*m3u8.MasterPlaylist); !ok {
+			return nil, errors.New("got m3u8 master playlist but data not valid")
+		} else if len(masterpl.Variants) == 0 {
+			return nil, errors.New("got m3u8 master playlist but zero variants found")
+		} else {
+			allBw := make([]int, len(masterpl.Variants))
+			allResolutions := make(map[int]*m3u8.Variant)
+			fmt.Printf("%+v\n", masterpl.Variants[0])
+			for k, v := range masterpl.Variants {
+				allBw[k] = int(v.Bandwidth)
+				allResolutions[int(v.Bandwidth)] = v
+			}
+			sort.Sort(sort.Reverse(sort.IntSlice(allBw)))
+			bestVariant := allResolutions[allBw[0]]
+			var variantURL *url.URL
+			if !strings.Contains(bestVariant.URI, "http") {
+				var err error
+				variantURL, err = baseURL.Parse(bestVariant.URI)
+				if err != nil {
+					return nil, err
+				}
+			} else {
+				variantURL, err = url.Parse(bestVariant.URI)
+				if err != nil {
+					return nil, errors.New("got m3u8 master playlist but bestVariant.URI failed")
+				}
+			}
+			for k, _ := range allBw {
+				v := allResolutions[allBw[k]]
+				fmt.Printf("ProgramId=%v Resolution=%v Bandwidth=%v Codecs=%v FrameRate=%v\n",
+					v.ProgramId, v.Resolution, v.Bandwidth, v.Codecs, v.FrameRate)
+			}
+			log.Printf("got m3u8 master playlist, use best variant resolution=%v URL=%s", bestVariant.Resolution, variantURL.String())
+			return parseHlsSegments(variantURL.String())
+		}
+	}
+
 	if t != m3u8.MEDIA {
 		return nil, errors.New("No support the m3u8 format")
 	}
